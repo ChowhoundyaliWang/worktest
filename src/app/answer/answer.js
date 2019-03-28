7d837ed57193685c71d86f2f62cc979b9e8e58e6
ChowhoundyaliWang <842102581@qq.com>
2019-03-28 17:52:51 +0800

    完成一个测试题

--- src/app/answer/answer.js
+++ src/app/answer/answer.js
@@ -7,7 +7,10 @@ export class AppAnswer extends App {
    * @private
    */
   onSuccess(value) {
+    console.log(value);
     // TODO: 在结果框（#result）中显示结果，并隐藏错误框（#error)
+    this.txtResult.innerHTML = value;
+    this.txtErr.style.display = 'none';
     console.log('字符串转换成功，别忘了将结果显示在#result元素中');
   }
   /**
@@ -16,6 +19,10 @@ export class AppAnswer extends App {
   onError(err) {
     // TODO: 显示错误框（#error）并在其中显示错误信息，
     //       清空结果框（#result）中的信息
+    console.log(err);
+    this.txtResult.innerHTML = '';
+    this.txtErr.innerHTML = err.message;
+    this.txtErr.style.display = 'block';
     console.error('请将该错误显示在#error元素中:', err.message);
   }
 }
@@ -34,5 +41,25 @@ async function castSpell(input) {
   // TODO: 在这里编写字符串转换逻辑
   // 将可能的错误通过 throw new Error('错误信息')抛出异常
   // 若输入能够正确处理，返回结果字符串
+  let specialIndArr = [];
+  if(input.includes(SPECIAL)){
+    // 找出特殊字符的下标
+    let n = 0;
+    while(n < input.length){
+      if(input[n] == SPECIAL){
+        specialIndArr.push(n)
+      }
+      n++;
+    }
+    const reg = new RegExp(SPECIAL, 'g');
+    let newInputArr = input.replace(reg, '').split('').reverse();
+    specialIndArr.forEach((item) => {
+      newInputArr.splice(item, 0, MAGIC_CHAR);
+    })
+    return newInputArr.join('');
+  }else{
+    return input.split('').reverse().join('');
+  }
+
   throw new Error('请实现 castSpell() 函数');
 }
